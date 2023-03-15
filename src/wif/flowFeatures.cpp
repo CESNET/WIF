a1f916fdeca4133776c93c5241c5a81b5b13bfd8
Richard Plny <plnyrich@fit.cvut.cz>
2023-03-15 15:09:08 +0100

    WIF - flows - introduce flow features for flow representation

---
+++ src/wif/flowFeatures.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @author Richard Plny <plnyrich@fit.cvut.cz>
+ * @brief Flow features implementation
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "wif/flowFeatures.hpp"
+
+namespace WIF {
+
+FlowFeatures::FlowFeatures(size_t expectedFeatureCount)
+{
+	m_features.resize(expectedFeatureCount);
+}
+
+size_t FlowFeatures::size() const
+{
+	return m_features.size();
+}
+
+void FlowFeatures::clear()
+{
+	m_features.clear();
+}
+
+} // namespace WIF
